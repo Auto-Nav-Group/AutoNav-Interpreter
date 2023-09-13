3cf9fd2ddfe90bcfe1bab743080cb73943d2c05c
Pillage_ <pillageyt@gmail.com>
2023-09-13 21:01:59 +0000

    init

---
+++ src/main/java/net/autonav/Odometry.java
@@ -0,0 +1,26 @@
+package net.autonav;
+
+public class Odometry {
+    //TODO: get position relative to 0, 0 (april tags)
+    private float x;
+    private float y;
+    private float theta;
+
+    public Odometry(final float x, final float y, final float theta) {
+        this.x = x;
+        this.y = y;
+        this.theta = theta;
+    }
+
+    public float getX() {
+        return this.x;
+    }
+
+    public float getY() {
+        return this.y;
+    }
+
+    public float getTheta() {
+        return this.theta;
+    }
+}
