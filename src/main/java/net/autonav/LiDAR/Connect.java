e97754cb20564a15aea57bf17c300c7a3ddd0553
PillageDev <63556914+PillageDev@users.noreply.github.com>
2023-09-21 14:44:52 -0400

    LiDAR Connection

--- src/main/java/net/autonav/LiDAR/Connect.java
+++ src/main/java/net/autonav/LiDAR/Connect.java
@@ -1,14 +1,29 @@
 package net.autonav.LiDAR;
 
-public class Connect {
-    String lidarIP = "10.67.2.184";
-    int lidarPort = 25565;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
-    try (
-        Socket lidarSocket = new Socket(lidarIP, lidarPort);
-        PrintWriter out = new PrintWriter(lidarSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(lidarSocket.getInputStream()));
-        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
-    )
+public class Connect {
+    Socket lidarSocket;
+    String lidarIP = "10.67.2.184";
+    final int lidarPort = 10940;
+    boolean connected = false;
     
+    private void connectToLidar() {
+        try {
+            ServerSocket serverSocket = new ServerSocket(lidarPort);
+            lidarSocket = serverSocket.accept();
+            
+            InputStream input = lidarSocket.getInputStream();
+            OutputStream output = lidarSocket.getOutputStream();
+            connected = true;
+        } catch(Exception e) {
+            System.out.println("Error connecting to LiDAR: " + e.getMessage());
+        }
+    }
 }
