72436eb8264720fe6432cf5e8816f453dddcfe7f
PillageDev <63556914+PillageDev@users.noreply.github.com>
2023-10-18 13:18:55 -0400

    Making sure that value is not null with constructor

--- src/main/java/net/autonav/Records/Command.java
+++ src/main/java/net/autonav/Records/Command.java
@@ -3,6 +3,10 @@ package net.autonav.Records;
 //TODO: Fix this mess of a file
 public record Command(Subsystems subsystem, IntCommands command, IntActions action, int value) {
     Command(Subsystems subsystem, IntCommands command, IntActions action) {
-        this(subsystem, command, action, -1);
+        if (command == IntCommands.SET) {
+            throw new IllegalArgumentException("Warning! Command is SET but no value was provided!");
+        } else {
+            this(subsystem, command, action, -1);
+        }
     }
 }
