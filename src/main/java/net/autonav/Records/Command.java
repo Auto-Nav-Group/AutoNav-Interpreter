86cabc1babb1935289416362e846094afadb75ac
PillageDev <63556914+PillageDev@users.noreply.github.com>
2023-10-18 16:45:56 +0000

    add comment to self

--- src/main/java/net/autonav/Records/Command.java
+++ src/main/java/net/autonav/Records/Command.java
@@ -1,5 +1,6 @@
 package net.autonav.Records;
 
+//TODO: Fix this mess of a file
 public record Command(Subsystems subsystem, IntCommands command, IntActions action, int value) {
     Command(Subsystems subsystem, IntCommands command, IntActions action) {
         this(subsystem, command, action, -1);
