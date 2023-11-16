de4c5b4b70b293f6b49fdd1f6c57acad792ee319
PillageDev <63556914+PillageDev@users.noreply.github.com>
2023-11-16 15:00:37 -0500

    More WPI

---
+++ src/main/java/net/autonav/HTTP/ProcessMovement.java
@@ -0,0 +1,8 @@
+public class ProcessMovement implements Runnable {
+    @Override 
+    public void run() {
+        while (true) {
+            // take the odometry from a queue
+        }
+    }
+}
